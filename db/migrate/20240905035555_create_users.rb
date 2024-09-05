0a75eb1de4ae619c2bb7ae59e1db1fe456c97c40
Dara Sroin <watchhme9@gmail.com>
2024-09-05 11:00:31 +0700

    time to consider about the model part, i pulled up the User model stuff in order to deal with the upcoming data input of the user

---
+++ db/migrate/20240905035555_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :firstname
+      t.string :lastname
+      t.datetime :birthday
+      t.string :email
+      t.string :phonenumber
+      t.string :subject
+
+      t.timestamps
+    end
+  end
+end
